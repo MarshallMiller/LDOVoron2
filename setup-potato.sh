93ea9bf7610b687376308b6cff6b8605b0e4c17d
Marshall Miller <joshmiller@hotmail.com>
2023-04-24 22:40:03 -0400

    updated scripts to get raspbian running on le potato

---
+++ setup-potato.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# turn off crowsnest
+sudo systemctl disable crowsnest.service
+
+# install klipper config files
+cp Firmware/*.cfg ~/printer_data/config/
+
+# make the system auto-login the pi user
+#sudo raspi-config nonint do_boot_behaviour B2
+
+# install KlipperScreen
+#cd ~
+#git clone https://github.com/jordanruthe/KlipperScreen.git
+#cd KlipperScreen
+#./scripts/KlipperScreen-install.sh
+
+sudo cp wlan0 /etc/network/interfaces.d/
